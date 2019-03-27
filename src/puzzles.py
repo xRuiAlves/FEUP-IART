f19ecc6316b89fbf793fb9a30e102080cf724d58
xRuiAlves <up201606746@fe.up.pt>
2019-03-27 15:09:04 +0000

    Added puzzles database

---
+++ src/puzzles.py
@@ -0,0 +1,36 @@
+easyPuzzles = {
+    'easy-a': 'BBBoFoooooFoAADoFoooDoooooEoooooECCC',
+    'easy-b': 'BBBooxFooooHFAAooHGoooooGDDEEEoooooo',
+    'easy-c': 'BBBooxFooooHFAAooHGoooooGDDDEEoooooo',
+    'easy-d': 'BBooGooEooGooEAAGooEoCCooFoooooFDDDo',
+    'easy-e': 'BBoooooGxDDooGAAIooHooIooHEEFFoooooo',
+    'easy-f': 'oxCCDDoooooJoGAAoJoGoIEEoHoIoooHFFFo',
+    'easy-g': 'oxCCCooooGoooAAGooooFDDxooFoooooFooo',
+    'easy-h': 'oICCDDHIooKoHxAAKoHEEJFFoooJoooGGGoo'
+}
+
+mediumPuzzles = {
+    'medium-10-a': 'oooxoooooHoIEAAHoIEFCCCooFGoooDDGooo',
+    'medium-10-b': 'ooooGoDoooGIDAAFHIooEFHoooEFBBoCCCoo',
+    'medium-10-c': 'GBBBoxGoDDoIAAoHoIoooHEEoooHooooFFoo',
+    'medium-10-d': 'FBBoKoFoIoKLAAIooLooJCCoGHJoooGHDDEE',
+    'medium-10-e': 'ooIBBBooIJoLoAAJKLoxDDKoHoEEooHFFFxo',
+    'medium-10-f': 'ooGHBBFoGHIJFAAoIJoCCooooooxoxoooooo',
+    'medium-10-g': 'HoIBBoHoIJoKHAAJoKCCoooooxEEoxGGoooo',
+    'medium-10-h': 'EoooooEooHBBEAAHoIoFGCCIoFGooJDDDooJ',
+    'medium-10-i': 'ooHoBBooHCCCFAAoIJFGDDIJoGooIxoooooo',
+    'medium-10-j': 'ooBBBxooJoKLAAJoKLIDDxooIFFoooGGHHoo'
+}
+
+hardPuzzles = {
+    'hard-10-a': 'BBHoKLooHoKLAAIooMoGICCMFGoJDDFEEJoo',
+    'hard-10-b': 'BBJoooHoJxLxHAAKLoEEoKoMoIFFoMoIGGoo',
+    'hard-10-c': 'oIBBLMoICCLMAAoKooHDDKooHoJxoxGGJooo',
+    'hard-10-d': 'oHBBoxoHIooLAAIooLGoIDDoGEEJKooFFJKo',
+    'hard-10-e': 'oGBBoKoGHooKAAHIoKFoHICCFDDoJooEEoJo',
+    'hard-10-f': 'oIBBxooIDDoLAAJooLHoJEEoHFFoKoGGGoKo',
+    'hard-10-g': 'IJBBCCIJDDxoIAALoooooLFFooKGGMHHKooM',
+    'hard-10-h': 'BBIooLoHIoxLoHAAKMDDoJKMGooJEEGFFFoo',
+    'hard-10-i': 'BBCCLooxEELMAAJooMIoJFFMIGGKoooHHKoo',
+    'hard-10-j': 'GBBoKLGHooKLGHIAAMCCIJoMoooJDDEEFFoo'
+}
