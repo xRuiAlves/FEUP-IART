d5192fa1d32f19411039aa1617f63640d842c32c
xRuiAlves <up201606746@fe.up.pt>
2019-03-29 15:28:40 +0000

    Fixed puzzle naming

--- src/puzzles.py
+++ src/puzzles.py
@@ -1,44 +1,42 @@
 easyPuzzles = {
-    'easy-a': 'BBBoFoooooFoAADoFoooDoooooEoooooECCC',
-    'easy-b': 'BBBooxFooooHFAAooHGoooooGDDEEEoooooo',
-    'easy-c': 'BBBooxFooooHFAAooHGoooooGDDDEEoooooo',
-    'easy-d': 'BBooGooEooGooEAAGooEoCCooFoooooFDDDo',
-    'easy-e': 'BBoooooGxDDooGAAIooHooIooHEEFFoooooo',
-    'easy-f': 'oxCCDDoooooJoGAAoJoGoIEEoHoIoooHFFFo',
-    'easy-g': 'oxCCCooooGoooAAGooooFDDxooFoooooFooo',
-    'easy-h': 'oICCDDHIooKoHxAAKoHEEJFFoooJoooGGGoo'
+    'easy-1': 'BBBoFoooooFoAADoFoooDoooooEoooooECCC',
+    'easy-2': 'BBBooxFooooHFAAooHGoooooGDDEEEoooooo',
+    'easy-3': 'BBBooxFooooHFAAooHGoooooGDDDEEoooooo',
+    'easy-4': 'BBooGooEooGooEAAGooEoCCooFoooooFDDDo',
+    'easy-5': 'BBoooooGxDDooGAAIooHooIooHEEFFoooooo',
+    'easy-6': 'oxCCDDoooooJoGAAoJoGoIEEoHoIoooHFFFo',
+    'easy-7': 'oxCCCooooGoooAAGooooFDDxooFoooooFooo',
+    'easy-8': 'oICCDDHIooKoHxAAKoHEEJFFoooJoooGGGoo'
 }
 
 mediumPuzzles = {
-    'medium-10-a': 'oooxoooooHoIEAAHoIEFCCCooFGoooDDGooo',
-    'medium-10-b': 'ooooGoDoooGIDAAFHIooEFHoooEFBBoCCCoo',
-    'medium-10-c': 'GBBBoxGoDDoIAAoHoIoooHEEoooHooooFFoo',
-    'medium-10-d': 'FBBoKoFoIoKLAAIooLooJCCoGHJoooGHDDEE',
-    'medium-10-e': 'ooIBBBooIJoLoAAJKLoxDDKoHoEEooHFFFxo',
-    'medium-10-f': 'ooGHBBFoGHIJFAAoIJoCCooooooxoxoooooo',
-    'medium-10-g': 'HoIBBoHoIJoKHAAJoKCCoooooxEEoxGGoooo',
-    'medium-10-h': 'EoooooEooHBBEAAHoIoFGCCIoFGooJDDDooJ',
-    'medium-10-i': 'ooHoBBooHCCCFAAoIJFGDDIJoGooIxoooooo',
-    'medium-10-j': 'ooBBBxooJoKLAAJoKLIDDxooIFFoooGGHHoo'
+    'medium-1': 'oooxoooooHoIEAAHoIEFCCCooFGoooDDGooo',
+    'medium-2': 'ooooGoDoooGIDAAFHIooEFHoooEFBBoCCCoo',
+    'medium-3': 'GBBBoxGoDDoIAAoHoIoooHEEoooHooooFFoo',
+    'medium-4': 'FBBoKoFoIoKLAAIooLooJCCoGHJoooGHDDEE',
+    'medium-5': 'ooIBBBooIJoLoAAJKLoxDDKoHoEEooHFFFxo',
+    'medium-6': 'ooGHBBFoGHIJFAAoIJoCCooooooxoxoooooo',
+    'medium-7': 'HoIBBoHoIJoKHAAJoKCCoooooxEEoxGGoooo',
+    'medium-8': 'EoooooEooHBBEAAHoIoFGCCIoFGooJDDDooJ',
+    'medium-9': 'ooHoBBooHCCCFAAoIJFGDDIJoGooIxoooooo',
+    'medium-10': 'ooBBBxooJoKLAAJoKLIDDxooIFFoooGGHHoo'
 }
 
 hardPuzzles = {
-    'hard-10-a': 'BBHoKLooHoKLAAIooMoGICCMFGoJDDFEEJoo',
-    'hard-10-b': 'BBJoooHoJxLxHAAKLoEEoKoMoIFFoMoIGGoo',
-    'hard-10-c': 'oIBBLMoICCLMAAoKooHDDKooHoJxoxGGJooo',
-    'hard-10-d': 'oHBBoxoHIooLAAIooLGoIDDoGEEJKooFFJKo',
-    'hard-10-e': 'oGBBoKoGHooKAAHIoKFoHICCFDDoJooEEoJo',
-    'hard-10-f': 'oIBBxooIDDoLAAJooLHoJEEoHFFoKoGGGoKo',
-    'hard-10-g': 'IJBBCCIJDDxoIAALoooooLFFooKGGMHHKooM',
-    'hard-10-h': 'BBIooLoHIoxLoHAAKMDDoJKMGooJEEGFFFoo',
-    'hard-10-i': 'BBCCLooxEELMAAJooMIoJFFMIGGKoooHHKoo',
-    'hard-10-j': 'GBBoKLGHooKLGHIAAMCCIJoMoooJDDEEFFoo'
+    'hard-1': 'BBHoKLooHoKLAAIooMoGICCMFGoJDDFEEJoo',
+    'hard-2': 'BBJoooHoJxLxHAAKLoEEoKoMoIFFoMoIGGoo',
+    'hard-3': 'oIBBLMoICCLMAAoKooHDDKooHoJxoxGGJooo',
+    'hard-4': 'oHBBoxoHIooLAAIooLGoIDDoGEEJKooFFJKo',
+    'hard-5': 'IJBBCCIJDDxoIAALoooooLFFooKGGMHHKooM',
+    'hard-6': 'BBIooLoHIoxLoHAAKMDDoJKMGooJEEGFFFoo',
+    'hard-7': 'BBCCLooxEELMAAJooMIoJFFMIGGKoooHHKoo',
+    'hard-8': 'GBBoKLGHooKLGHIAAMCCIJoMoooJDDEEFFoo'
 }
 
 longPuzzles = {
-    'long-a': 'BBBJCCHooJoKHAAJoKooIDDLEEIooLooxoGG',
-    'long-b': 'oooJBBoCCJLoAAIKLoDDIKEEHFFFoMHoGGoM',
-    'long-c': 'oJoxCCoJDDMooAALMoxoKLFFIoKGGNIoHHoN',
-    'long-d': 'IoBBBNICCKMNAAJKMoDDJLMxoooLFFoGGHHo',
-    'long-e': 'BBBoooooICCxAAIJLMHEEJLMHooKFFGGGKoo'
+    'long-1': 'BBBJCCHooJoKHAAJoKooIDDLEEIooLooxoGG',
+    'long-2': 'oooJBBoCCJLoAAIKLoDDIKEEHFFFoMHoGGoM',
+    'long-3': 'oJoxCCoJDDMooAALMoxoKLFFIoKGGNIoHHoN',
+    'long-4': 'IoBBBNICCKMNAAJKMoDDJLMxoooLFFoGGHHo',
+    'long-5': 'BBBoooooICCxAAIJLMHEEJLMHooKFFGGGKoo'
 }
