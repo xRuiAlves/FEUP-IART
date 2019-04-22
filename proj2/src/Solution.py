be377b964c3c4d3cace3d4f9d64603a358b4f2d2
xRuiAlves <up201606746@fe.up.pt>
2019-04-22 12:20:34 +0100

    Added solution representation and generation

---
+++ proj2/src/Solution.py
@@ -0,0 +1,25 @@
+import random
+
+NUM_DAYS = 5
+NUM_TIMESLOTS_PER_DAY = 9
+NUM_TIMESLOTS = NUM_DAYS * NUM_TIMESLOTS_PER_DAY
+
+
+# TODO: Discuss
+# After understanding the problem, the best way to represent a solution is an array with the events that are going to take place in each timeslot for each room.
+# e.g. if there were 2 days with 2 timeslots each, 4 timeslots total
+# if there were 3 rooms, there could be 12 events taking place in total (4 timeslots * 3 rooms)
+
+class Solution:
+    def __init__(self, num_events, num_rooms, solution=[]):
+        self.solution = solution
+        self.num_events = num_events
+        self.num_rooms = num_rooms
+        if (len(self.solution) == 0):
+            self.randomize()
+
+    def randomize(self):
+        self.solution = [random.randint(0, self.num_events - 1) for i in range(NUM_TIMESLOTS * self.num_rooms)]
+
+    def __str__(self):
+        return "{}".format(self.solution)
--- proj2/src/main.py
+++ proj2/src/main.py
@@ -4,6 +4,7 @@ from Student import Student
 from Event import Event
 from Room import Room
 from FileReader import FileReader
+from Solution import Solution
 
 def main():
     if (len(sys.argv) < 2):
@@ -14,6 +15,12 @@ def main():
     input_file = sys.argv[1]
     f = FileReader()
     f.readFile(input_file)
+
+    # Generate a random solution (temporary)
+    print("\nRandom solution example:")
+    s = Solution(len(f.events), len(f.rooms))
+    print(s)
+
 
 
 # Entry point
